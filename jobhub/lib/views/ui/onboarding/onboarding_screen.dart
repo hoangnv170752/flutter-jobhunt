2e7ef7e15c7df72951e8ca880474c5454346556d
hoangnv170752 <hoang.nv.ral@gmail.com>
2023-07-22 11:07:40 +0700

    set up jobhunt onboarding

--- jobhub/lib/views/ui/onboarding/onboarding_screen.dart
+++ jobhub/lib/views/ui/onboarding/onboarding_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:jobhub/controllers/onboarding_provider.dart';
 import 'package:jobhub/views/common/exports.dart';
-
+import 'package:jobhub/views/ui/onboarding/widgets/page_one.dart';
+import 'package:jobhub/views/ui/onboarding/widgets/page_three.dart';
+import 'package:jobhub/views/ui/onboarding/widgets/page_two.dart';
+import 'package:provider/provider.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -8,8 +12,23 @@ class OnBoardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ReusableText(text: "OnBoardingScreen", style: appstyle(30, Color(kDark.value), FontWeight.bold),),
+      body: Consumer<OnBoardNotifier>(
+        builder: (context, onBoardNotifier, child) {
+          return Stack(
+            children: [
+              PageView(
+                physics: onBoardNotifier.isLastPage
+                    ? const NeverScrollableScrollPhysics()
+                    : const AlwaysScrollableScrollPhysics(),
+                children: const [
+                  PageOne(),
+                  PageTwo(),
+                  PageThree(),
+                ],
+              )
+            ],
+          );
+        },
       ),
     );
   }
